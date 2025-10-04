9a5a80eb523066fc0d7fb7d7481316d381a84b0d
Danielmbugua <platinumwriter2022@gmail.com>
2025-10-04 12:06:15 +0300

    changed account number

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,12 +1,12 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         """Initialize the account with an optional initial balance (default is 0)."""
-        self.__account_balance = initial_balance  # private attribute for encapsulation
+        self.account_balance = initial_balance  # private attribute for encapsulation
 
     def deposit(self, amount):
         """Deposit money into the account."""
         if amount > 0:
-            self.__account_balance += amount
+            self.account_balance += amount
         else:
             print("Deposit amount must be positive.")
 
@@ -15,12 +15,12 @@ class BankAccount:
         if amount <= 0:
             print("Withdrawal amount must be positive.")
             return False
-        if self.__account_balance >= amount:
-            self.__account_balance -= amount
+        if self.account_balance >= amount:
+            self.account_balance -= amount
             return True
         else:
             return False
 
     def display_balance(self):
-        """Display the current balance."""
-        print(f"Current Balance: ${self.__account_balance}")
+        
+        print(f"Current Balance: ${self.account_balance:.2f}")
