bdb8d838cc27e48eb3a70b1b546fcfee964d4a75
Danielmbugua <platinumwriter2022@gmail.com>
2025-10-11 11:31:09 +0300

    added class_static

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        """Static method: works independently of any class or instance data"""
+        return a + b
+    @classmethod
+    def multiply(cls, a, b):
+        """Class method: has access to the class itself through 'cls"""
+        print(f"Calculation_type: {cls.calculation_type}")
+        return a *b
